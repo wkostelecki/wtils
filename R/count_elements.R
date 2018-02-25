99cbbfc0834db77b959e86355f9f003cb687af50
Wojtek Kostelecki <wkostelecki@mango-solutions.com>
2018-02-25 00:56:41 +0000

    added option to count elements while allowing for quoted values

--- R/count_elements.R
+++ R/count_elements.R
@@ -1,5 +1,14 @@
+#' count_elements
+#' @param x A character vector. Usually, the output from \code{readLines()}.
+#' @param sep A separator used in \code{x}.
+#' @examples
+#' text = c("a,b", "b,d", 'a,",,d"', "a, b, c, d")
+#' count_elements(text)
 #' @export
-count_elements = function(x, sep = ",") {
+count_elements = function(x, sep = ",", quote = NULL) {
+  if (!is.null(quote)) {
+    x = gsub(paste0(quote, ".*?", quote), "", x)
+  }
   x = paste0(x, sep, "x")
   sapply(strsplit(x, sep), length) - 1
 }
