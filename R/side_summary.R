3bfe5b68f4529b026728e444ad00ad3fd02021ae
Wojtek Kostelecki <wkostelecki@mango-solutions.com>
2018-04-15 09:24:55 +0100

    refactored side_summary

--- R/side_summary.R
+++ R/side_summary.R
@@ -1,35 +1,48 @@
 #' side_summary
 #' @export
 #' @param data A data.frame.
-#' @param ... Extra inputs.
 #'
 #' @examples
 #' side_summary(mtcars)
-#' side_summary(mtcars, unique = T)
-#' side_summary(mtcars, unique = T, sample = T)
-side_summary = function(data, ..., unique = FALSE, sample = FALSE){
+#' side_summary(mtcars, extra = "unique")
+#' side_summary(mtcars, extra = c("unique", "sample"))
+#'
+#' library(dplyr)
+#' mtcars %>% mutate(n = rnorm(n()), x = runif(n()) > 0.5) %>% side_summary
+#' mtcars %>% mutate(vs = as.character(vs)) %>% side_summary
+side_summary = function(data, extra = NULL) {
 
-  summaries = sapply(data, function(x) column_summary(x, ...),
+  summaries = sapply(data, function(x) column_summary(x),
                      simplify = FALSE)
 
   out = cbind(Column = names(summaries),
-              Class = unname(sapply(data, function(x) paste(class(x), collapse = ", "))),
+              Class = unname(sapply(data, function(x) paste(class(x),
+                                                            collapse = ", "))),
               dplyr::bind_rows(unname(summaries)))
 
-  if (unique | sample) {
-    unique_vals = lapply(data, unique)
+  if (any("unique" == extra) | any("sample" == extra)) {
+    unique_vals = lapply(data,
+                         function(x) {
+                           dplyr::count(data.frame(x = x,
+                                                   stringsAsFactors = FALSE),
+                                        x)
+                         })
   }
 
-  if (unique) {
+  if (any("unique" == extra)) {
     out = cbind(out,
-                Unique = unname(sapply(unique_vals, function(x) length(x))))
+                Unique = unname(sapply(unique_vals, function(x) nrow(x))))
   }
 
-  if (sample) {
+  if (any("sample" == extra)) {
     out = cbind(out,
-                Sample = unname(sapply(unique_vals, function(x) {
-                  paste(sample(x, min(5, length(x))), collapse = ", ")
-                })))
+                Sample = unname(sapply(
+                  unique_vals,
+                  function(x) {
+                    paste(sample(paste0(x[["x"]], " (", x[["n"]], ")"), min(5, nrow(x))), collapse = ", ")
+                  }
+                ))
+    )
   }
 
   out
@@ -47,17 +60,33 @@ column_summary.default = function(x, ...){
              Mean = NA_real_,
              Max = NA_real_,
              Sum = NA_real_,
+             Non0 = NA_real_,
+             gt0 = NA_real_,
              NAs = sum(is.na(x)),
              stringsAsFactors = FALSE)
 
 }
 
 column_summary.numeric = function(x, ...){
-  data.frame(Min = signif(min(x, na.rm = TRUE), 3),
-             Med = signif(median(x, na.rm = TRUE), 3),
-             Mean = signif(mean(x, na.rm = TRUE), 3),
-             Max = signif(max(x, na.rm = TRUE), 3),
+  data.frame(Min = min(x, na.rm = TRUE),
+             Med = median(x, na.rm = TRUE),
+             Mean = mean(x, na.rm = TRUE),
+             Max = max(x, na.rm = TRUE),
              Sum = sum(as.numeric(x), na.rm = TRUE),
-             NAs = signif(sum(is.na(x)), 3),
+             Non0 = sum(x != 0, na.rm = TRUE),
+             gt0 = sum(x > 0, na.rm = TRUE),
+             NAs = sum(is.na(x)),
+             stringsAsFactors = FALSE)
+}
+
+column_summary.logical = function(x, ...){
+  data.frame(Min = min(x, na.rm = TRUE),
+             Med = median(as.numeric(x), na.rm = TRUE),
+             Mean = mean(x, na.rm = TRUE),
+             Max = max(x, na.rm = TRUE),
+             Sum = sum(x, na.rm = TRUE),
+             Non0 = sum(x, na.rm = TRUE),
+             gt0 = sum(x, na.rm = TRUE),
+             NAs = sum(is.na(x)),
              stringsAsFactors = FALSE)
 }
--- tests/testthat/test-side_summary.R
+++ tests/testthat/test-side_summary.R
@@ -8,11 +8,13 @@ test_that("side_summary works", {
                                     Y = c(1.5, NA),
                                     Z = Z))
 
-  expect_equal(as.character(summary$Column), c("X", "Y", "Z"))
-  expect_equal(summary$Min, c("1", "1.5", NA))
-  expect_equal(summary$Med, c("1", "1.5", NA))
-  expect_equal(summary$Mean, c("1", "1.5", NA))
-  expect_equal(summary$Max, c("1", "1.5", NA))
-  expect_equal(summary$NAs, c("1", "1", "1"))
+  expect_equal(as.character(summary[["Column"]]), c("X", "Y", "Z"))
+  expect_equal(as.character(summary[["Class"]]),
+               c("integer", "numeric", "factor"))
+  expect_equal(summary[["Min"]], c(1, 1.5, NA))
+  expect_equal(summary[["Med"]], c(1, 1.5, NA))
+  expect_equal(summary[["Mean"]], c(1, 1.5, NA))
+  expect_equal(summary[["Max"]], c(1, 1.5, NA))
+  expect_equal(summary[["NAs"]], c(1, 1, 1))
 
 })
