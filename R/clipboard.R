2eb5ae4911188469eccee27d1e7c61fc1a18a0a2
Wojtek Kostelecki <wkostelecki@mango-solutions.com>
2018-05-10 22:54:59 +0100

    reference to utils functions

--- R/clipboard.R
+++ R/clipboard.R
@@ -12,15 +12,15 @@
 #' print(clipboard())
 clipboard = function (x = NULL, alloc = 2048) {
   if (is.null(x)) {
-    read.table("clipboard", header = TRUE, sep = "\t")
+    utils::read.table("clipboard", header = TRUE, sep = "\t")
   } else {
     stopifnot(is.data.frame(x))
     stopifnot(is.numeric(alloc))
     stopifnot(log2(alloc) == round(log2(alloc)))
-    write.table(x,
-                sprintf("clipboard-%d", alloc),
-                sep = "\t",
-                row.names = FALSE,
-                qmethod = "double")
+    utils::write.table(x,
+                       sprintf("clipboard-%d", alloc),
+                       sep = "\t",
+                       row.names = FALSE,
+                       qmethod = "double")
   }
 }
