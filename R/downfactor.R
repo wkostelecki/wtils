3553a8bdd6b33fa75bb330d6d2ee255d7d197fe3
Wojtek Kostelecki <wkostelecki@mango-solutions.com>
2018-06-08 16:33:49 +0100

    improved documentation

--- R/downfactor.R
+++ R/downfactor.R
@@ -1,10 +1,17 @@
 #' downfactor
-#' @param x A character or factor vector
-#' @param y A numeric vector
-#' @param n Number of factors to retain
+#' @description Retains/sorts the \code{n} largest levels of a character or
+#'   factor vector and converts the remaining levels to \code{"Other"}.
+#' @param x A character or factor vector.
+#' @param y A numeric vector used for aggregating and ordering factor levels.
+#' @param n Number of factor levels to retain.
+#' @examples
+#' downfactor(letters, n = 10)
+#' downfactor(letters, seq_along(letters), n = 10)
 #' @export
 downfactor = function(x, y = rep(1, length(x)), n = 5) {
-  fct = reorder(factor(x), -y, function(x) sum(x, na.rm = TRUE))
-  n = pmin(n, length(levels(fct)))
-  forcats::fct_other(fct, levels(fct)[seq_len(n)])
+  fct = forcats::fct_reorder(factor(x), -y, function(x) sum(x, na.rm = TRUE))
+  if (n < length(levels(fct))) {
+    fct = forcats::fct_other(fct, levels(fct)[seq_len(n)])
+  }
+  fct
 }
