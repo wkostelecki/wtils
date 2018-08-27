5af03410a21a28003a97ed03cf69888a3738b572
Wojtek Kostelecki <wkostelecki@mango-solutions.com>
2018-08-27 12:19:33 +0100

    open file path containing cursor

--- R/open_file.R
+++ R/open_file.R
@@ -3,10 +3,20 @@ open_file_addin = function () {
   if (Sys.getenv("RSTUDIO") != "1") {
     stop("Only works in RStudio")
   }
-  context = rstudioapi::getActiveDocumentContext()
-  file = gsub("^ *| *$", "", context[["selection"]][[1]][["text"]])
-  file = gsub("'|\"", "", file)
-  file = gsub("/*$|\\\\*$", "", file)
+  context = rstudioapi::getSourceEditorContext()
+  line = context$contents[context$selection[[1]]$range$start[1]]
+  start = sub('^.*?"', "",
+              substring(line, 1, context$selection[[1]]$range$start[[2]]))
+  end = sub('".*?$', "",
+            substring(line,
+                      context$selection[[1]]$range$start[[2]],
+                      nchar(line)))
+  if (nchar(start) > 0) {
+    start = substring(start, 1, nchar(start) - 1)
+  } else if (nchar(end) > 0) {
+    end = substring(end, 2, nchar(end))
+  }
+  file = paste0(start, end)
   if (!file.exists(file)) {
     warning("File does not exist.")
     return(invisible(NULL))
@@ -19,3 +29,7 @@ open_file = function(file){
   system(sprintf("open \"%s\"", file))
   invisible(NULL)
 }
+
+
+
+# "R/count_elements.R"
