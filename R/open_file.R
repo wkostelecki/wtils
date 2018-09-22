7bf45d1e55e7e0d6cb6a0a5e929018b4d79eb7e2
Wojtek Kostelecki <wkostelecki@mango-solutions.com>
2018-09-22 20:16:17 +0100

    fixed open_file_addin when single quotes are used

--- R/open_file.R
+++ R/open_file.R
@@ -1,27 +1,34 @@
+
 #' @export
+#' @examples
+#' "inst/extdata/"
+#' 'inst/extdata/read_excel2.xlsx'
 open_file_addin = function () {
+
   if (Sys.getenv("RSTUDIO") != "1") {
     stop("Only works in RStudio")
   }
+
   context = rstudioapi::getSourceEditorContext()
   line = context$contents[context$selection[[1]]$range$start[1]]
-  start = sub('^.*?"', "",
-              substring(line, 1, context$selection[[1]]$range$start[[2]]))
-  end = sub('".*?$', "",
-            substring(line,
-                      context$selection[[1]]$range$start[[2]],
-                      nchar(line)))
-  if (nchar(start) > 0) {
-    start = substring(start, 1, nchar(start) - 1)
-  } else if (nchar(end) > 0) {
-    end = substring(end, 2, nchar(end))
-  }
+
+  start1 = substring(line, 1, context$selection[[1]]$range$start[[2]] - 1)
+  end1 = substring(line,
+                   context$selection[[1]]$range$start[[2]],
+                   nchar(line))
+
+  start = sub("^.*[\"']", "", start1)
+  end = sub("[\"'].*$", "", end1)
+
   file = paste0(start, end)
-  if (!file.exists(file)) {
+
+  if (!file.exists(file) & !dir.exists(file)) {
     warning("File does not exist.")
     return(invisible(NULL))
   }
+
   open_file(file)
+
 }
 
 open_file = function(file){
@@ -29,7 +36,3 @@ open_file = function(file){
   system(sprintf("open \"%s\"", file))
   invisible(NULL)
 }
-
-
-
-# "R/count_elements.R"
